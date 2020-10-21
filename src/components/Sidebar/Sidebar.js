e53eaf68519204a67f57e639b6c944f00fda933f
Maria Gocal <mgocalkappos@luc.edu>
2020-10-21 14:32:08 -0500

    Corrected typos

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Sidebar.module.css';
+
+const Sidebar = (props) => {
+    let {  } = props;
+
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+Searchbar.propTypes = {
+
+};
+
+Searchbar.defaultProps = {
+  
+};
+
+
+
+
+export default Searchbar;
