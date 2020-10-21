4c31c19d723e41f2eb1b7e37aac5ef4d791c6b6d
Maria Gocal <mgocalkappos@luc.edu>
2020-10-21 14:09:44 -0500

    Added home page basics

---
+++ src/routes/Home/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './Home.module.css';
+
+export default class Home extends Component {
+  render() {
+      return (
+        <div>
+          <h1>Calendar App</h1>
+        </div>
+      );
+  }
+}
