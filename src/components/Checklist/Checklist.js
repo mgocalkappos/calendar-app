19550ff09faffbc6242df844c2c62197c528d248
Maria Gocal <mgocalkappos@luc.edu>
2020-11-17 13:43:51 -0600

    Made trash can icon only visible on hover

--- src/components/Checklist/Checklist.js
+++ src/components/Checklist/Checklist.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Checklist.module.css';
+import ListItem from '../../components/ListItem';
 
 const Checklist = (props) => {
     let {  } = props;
 
-
     return (
       <div>
-        Checklist
+        <ListItem
+          label = "Do groceries"
+        />
       </div>
     );
 };
