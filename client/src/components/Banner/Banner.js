eab46defe74ca77ce4f1986729e05e3e266a4136
Maria Gocal <mgocalkappos@luc.edu>
2020-11-19 16:32:19 -0600

    Created initial structure for Banner component

---
+++ client/src/components/Banner/Banner.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Banner.module.css';
+
+const Banner = (props) => {
+    let {  } = props;
+
+    return (
+      <div>
+        Chronomy
+      </div>
+    );
+};
+
+Banner.propTypes = {
+
+};
+
+Banner.defaultProps = {
+
+};
+
+export default Banner;
