841ce921ceeb5227430e3e9912bc855dcd84feef
Maria Gocal <mgocalkappos@luc.edu>
2020-11-19 21:12:41 -0600

    Finalized home page layout

--- client/src/routes/Home/Home.js
+++ client/src/routes/Home/Home.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import styles from './Home.module.css';
+import Sidebar from '../../components/Sidebar';
+import Calendar from '../../components/Calendar';
+import Checklist from '../../components/Checklist';
+import Banner from '../../components/Banner';
 
 export default class Home extends Component {
   render() {
       return (
-        <div>
-          <h1>Calendar App</h1>
+        <div className={styles.pageLayout}>
+          <div className={styles.header}>
+            <Banner
+              calendarTitle = 'My Calendar'
+            />
+          </div>
+          <div className={styles.sidebar}>
+            <Sidebar width="15vw" height="87vh">
+                <h2> Calendar </h2>
+                <h2> Calendar 2 </h2>
+            </Sidebar>
+          </div>
+          <div className={styles.content}>
+            <div className={styles.toDoList}>
+              <Checklist
+                  title = 'To Do List'
+                />
+            </div>
+            <div className={styles.toBuyList}>
+              <Checklist
+                  title = 'To Buy List'
+                />
+            </div>
+            <div className={styles.calendar}>
+              <Calendar/>
+            </div>
+          </div>
         </div>
       );
   }
