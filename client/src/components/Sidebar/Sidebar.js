47b1a16797e51ac54645e31f710567b0c64072d7
Maria Gocal <mgocalkappos@luc.edu>
2020-11-19 22:52:08 -0600

    Added SidebarTab component to Sidebar and made it work with Express

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,8 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Sidebar.module.css';
+import SidebarTab from '../../components/SidebarTab';
 
-const Sidebar = (props) => {
+export default class Checklist extends Component {
+  constructor(props) {
+      super(props);
+
+      let { } = this.props;
+
+      this.state = {
+        calendars: []
+      };
+  }
+
+  static propTypes = {
+    tabs: PropTypes.string.isRequired, //for now
+    width: PropTypes.string,
+    height: PropTypes.string,
+  };
+
+  static defaultProps = {
+    width: "25vw",
+  };
+
+  componentDidMount() {
+    fetch('/api/calendars')
+      .then(res => res.json())
+      .then(calendars => this.setState({calendars}));
+  }
+
+  render() {
+        let { children, width, height } = this.props;
+
+        let {  } = this.state;
+
+        return (
+          <div className={styles.sidebar} style={{width: width, minHeight: height }}>
+            <div>
+                {this.state.calendars.map(calendars =>
+                  <div key={calendars.id}>
+                    <SidebarTab
+                      calendarTitle = {calendars.title}
+                      />
+                  </div>
+                )}
+            </div>
+            <div>
+              <SidebarTab
+                calendarTitle = '+'
+                />
+            </div>
+          </div>
+        );
+    }
+}
+
+{/*const Sidebar = (props) => {
     let { children, width, height } = props;
 
 
@@ -26,4 +80,4 @@ Sidebar.defaultProps = {
   width: "25vw",
 };
 
-export default Sidebar;
+export default Sidebar;*/}
