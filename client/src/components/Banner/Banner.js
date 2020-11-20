790646fcd4553bd549f820810be75d1ff57c61af
Maria Gocal <mgocalkappos@luc.edu>
2020-11-19 19:03:07 -0600

    Completed styling banner component

--- client/src/components/Banner/Banner.js
+++ client/src/components/Banner/Banner.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Banner.module.css';
+import logo from '../../assets/chronomy-logo.png';
 
 const Banner = (props) => {
-    let {  } = props;
+    let { calendarTitle } = props;
 
     return (
-      <div>
-        Chronomy
+      <div className = {styles.banner}>
+        <img
+          src={logo}
+          alt={"logo"}
+          className={styles.logo}
+        />
+        <h1 className = {styles.calendarTitle}> { calendarTitle } </h1>
       </div>
     );
 };
 
 Banner.propTypes = {
-
+ calendarTitle: PropTypes.string.isRequired,
 };
 
 Banner.defaultProps = {
-
+  calendarTitle: 'Untitled',
 };
 
 export default Banner;
